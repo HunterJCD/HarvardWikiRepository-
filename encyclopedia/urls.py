6324e39d3569f586afc59a4e8be90ca7be808d3e
Hunter Jones <Jones@DESKTOP-JUM36AE>
2022-07-12 18:39:47 -0400

    Added programmtic HTML converter

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("createnewpage", views.createnewpage, name="createnewpage"),
+    path("wiki/<str:title>", views.wiki, name="wiki")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from re import M
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -7,4 +9,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki(request, title):
+    markdowner = Markdown()
+   
+    print(f"Title: {title}")
+    print(util.get_entry(title))
+    return render (request, "encyclopedia/wiki.html", {
+        "Content": markdowner.convert(util.get_entry(title)), 
+        "Title": title
+        
+    })
+
+def createnewpage(request):
+    return render(request, "encyclopedia/createnewpage.html")
+
 
