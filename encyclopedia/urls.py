80ac1797f3628978f084e492319e27d146c40c34
Hunter Jones <Jones@DESKTOP-JUM36AE>
2022-07-12 22:09:59 -0400

    added create new pages and edit pages functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("createnewpage", views.createnewpage, name="createnewpage"),
+    path("RandomPage", views.RandomPage, name="RandomPage"),
+    path("editpages/<str:title>", views.editpages, name="editpages"),
     path("wiki/<str:title>", views.wiki, name="wiki")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,61 @@
 from re import M
 from django.shortcuts import render
 from markdown2 import Markdown
+from django import forms 
+from django.http import HttpResponseRedirect
+from django.urls import reverse 
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    entrytitle = forms.CharField(label= "New Entry")
+    textentry = forms.CharField(widget= forms.Textarea())
+
+
+def createnewpage(request):
+    print(" something is working ")
+    if request.method == "POST":
+        entry = NewEntryForm(request.POST)
+        if entry.is_valid():
+           entrytitle = entry.cleaned_data["entrytitle"]
+           if util.get_entry(entrytitle) != None :
+                return render (request, "encyclopedia/createnewpage.html", {
+                    "form": entry,
+                    "error": "An entry with this name already exists"
+                })
+           textentry = entry.cleaned_data["textentry"]
+           util.save_entry(entrytitle, textentry)
+           return HttpResponseRedirect(f"wiki/{entrytitle}")
+        else:
+            return render(request, "createnewpage.html", {
+                "form": entry
+            })
+    else: 
+        print("else statement ran")
+        return render(request, "encyclopedia/createnewpage.html", 
+        {
+            "form": NewEntryForm()
+        })
+    
+def editpages(request, title):
+    if request.method == "POST":
+        entry = NewEntryForm(request.POST)
+        if entry.is_valid():
+           entrytitle = entry.cleaned_data["entrytitle"]
+           textentry = entry.cleaned_data["textentry"]
+           util.save_entry(entrytitle, textentry)
+           return HttpResponseRedirect(f"/wiki/{entrytitle}")
+        else:
+            return render(request, "editpages.html", {
+                "form": entry,
+                "Title": title
+            })
+     
+        
+    return render(request, "encyclopedia/editpages.html", {
+       "form": NewEntryForm({ 'entrytitle': title, 'textentry': util.get_entry(title)}), 
+       "Title": title   })
+    
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,7 +73,10 @@ def wiki(request, title):
         
     })
 
-def createnewpage(request):
+def createnewpag(request):
     return render(request, "encyclopedia/createnewpage.html")
+
+def RandomPage(request):
+    return render( request, "encyclopedia/RandomPage.html")
 
 
